b85908f43a3f5118ccd8322e21e8dbca5220017a
ChaeeunPark <chaeun0618@naver.com>
2020-07-15 21:05:30 +0900

    first

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path 
-from .views import main, new, create, show 
-
+from .views import main, new, create, show, update, delete
 app_name = "posts" 
 urlpatterns = [    
     path('new/', new, name="new"),    
     path('create/', create, name="create"), 
     path('', main, name="main"),
     path('show/<int:id>', show, name="show"),
+    path('update/<int:id>/', update, name="update"),
+    path('delete/<int:id>/', delete, name="delete"), 
     ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from .models import Post
+from django.shortcuts import render, redirect, get_object_or_404 
+from .models import Post 
 
 def new(request):    
     return render(request, 'posts/new.html')
@@ -8,15 +8,30 @@ def create(request):
     if request.method == "POST":        
         title = request.POST.get('title')        
         content = request.POST.get('content')        
-        Post.objects.create(title=title, content=content)        
+        image = request.FILES.get('image')
+        Post.objects.create(title=title, content=content, image = image)        
         return redirect('main')   
 
 def main(request):    
     posts = Post.objects.all()    
     return render(request, 'posts/main.html', {'posts': posts})
 
-
 def show(request, id):    
     post = Post.objects.get(pk=id)    
     return render(request, 'posts/show.html', {'post': post})
 
+def update(request, id):
+    post = get_object_or_404(Post, pk=id)
+    if request.method == "POST": 
+        post.title = request.POST['title']
+        post.content = request.POST['content']
+        post.image = request.FILES.get('image') 
+        post.save()
+        return redirect('posts:show', post.id)
+    return render(request, 'posts/update.html', {'post':post})
+
+
+def delete(request, id): 
+	post = get_object_or_404(Post, pk=id) 
+	post.delete()
+	return redirect("posts:main")
